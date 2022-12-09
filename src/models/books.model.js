3113f345713f70cd436453e5ed84787bd607749b
sunilmsd007 <sunilmsd007@gmail.com>
2022-12-09 19:47:51 +0530

    [Sunilkumar]Modified: book schema and to add book details into cart

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -1,5 +1,31 @@
 import { Schema, model } from 'mongoose';
 
-const bookSchema = new Schema({}, { strict: false });
+const bookSchema = new Schema(
+    {
+        description: {
+            type: String
+        },
+        discountPrice: {
+            type: Number
+        },
+        bookImage: {
+            type: String
+        },
+        bookName: {
+            type: String
+        },
+        author: {
+            type: String
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        },
+        price: {
+            type: Number
+        },
 
-export default model('', bookSchema, "books");
+    }
+);
+
+export default model('Book', bookSchema, "books");
--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,27 +1,31 @@
 import Cart from '../models/cart.model';
 import Book from '../models/books.model';
 
-//register new user
+//add to cart
 export const addToCart = async (body) => {
     console.log("body============>",body)
-    const bookData = await Book.findById({ _id: body._id });
-    console.log("data============>",bookData._id)
+    const bookData = await Book.findOne({ _id: body._id });
+    console.log("bookdata============>",bookData)
+    console.log("dataId============>",bookData._id)
+    console.log("description============>",bookData.description)
     if(bookData!=null){
         const cartExist = await Cart.findOne({ userId: body.userId });
         console.log("cart============>",cartExist)
+        let addBook = {
+            productId: bookData._id,
+            description: bookData.description,
+            bookName: bookData.bookName,
+            bookImage: bookData.bookImage,
+            author: bookData.author,
+            price: bookData.price
+        };
         if(cartExist==null){
-            const createCart = await Cart.create({userId:body.userId});
-            console.log("data============>",bookData._id)
-            const updateCart = await Cart.findOneAndUpdate(
-                {userId:body.userId},
-                {$push:{books:{productId:bookData._id}}},
-                {new: true}
-                );
-                return updateCart;
+            const createCart = await Cart.create({userId:body.userId, books:[addBook], cart_total:bookData.price});
+            return createCart;
         }else{
             const updateCart = await Cart.findOneAndUpdate(
                 {_id:cartExist._id},
-                {$push:{books:{productId:bookData._id}}},
+                {$push:{books:[addBook]}},
                 {new: true}
                 );
                 return updateCart;
