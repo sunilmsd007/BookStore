f686d3b40c9322106aca0b65470806e3ff0e5060
sunilmsd007 <sunilmsd007@gmail.com>
2022-12-11 13:17:32 +0530

    [Sunilkumar]Modified: to update cart total and book quantity in cart

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -6,11 +6,11 @@ export const addToCart = async (body) => {
     console.log("body============>",body)
     const bookData = await Book.findOne({ _id: body._id });
     console.log("bookdata============>",bookData)
-    console.log("dataId============>",bookData._id)
-    console.log("description============>",bookData.description)
     if(bookData!=null){
         const cartExist = await Cart.findOne({ userId: body.userId });
         console.log("cart============>",cartExist)
+        let bookExist = false;
+        let totalCartPrice=0;
         let addBook = {
             productId: bookData._id,
             description: bookData.description,
@@ -19,16 +19,35 @@ export const addToCart = async (body) => {
             author: bookData.author,
             price: bookData.price
         };
+
         if(cartExist==null){
-            const createCart = await Cart.create({userId:body.userId, books:[addBook], cart_total:bookData.price});
+            const createCart = await Cart.create({userId:body.userId, books:[addBook], cart_total:addBook.price});
             return createCart;
         }else{
-            const updateCart = await Cart.findOneAndUpdate(
-                {_id:cartExist._id},
-                {$push:{books:[addBook]}},
-                {new: true}
+            const bookfind = cartExist.books.forEach(element => {
+                if(element.productId==body._id){
+                   element.quantity = element.quantity + 1;
+                   totalCartPrice = element.price + cartExist.cart_total;
+                   bookExist = true;
+                } 
+            });
+            if(bookExist == true){
+                const updateCart = await Cart.findOneAndUpdate(
+                    {_id:cartExist._id},
+                    {books:cartExist.books, cart_total: totalCartPrice},
+                    {new: true}
+                    );
+                    return updateCart;
+            }else{
+                cartExist.cart_total += addBook.price;
+                const updateCart = await Cart.findByIdAndUpdate(
+                    {_id:cartExist._id},
+                    {$push: {books:[addBook]}, cart_total: cartExist.cart_total},
+                    {new: true}
                 );
                 return updateCart;
+            }
+            
         }
     }else{
         throw new Error("Book is not available")
